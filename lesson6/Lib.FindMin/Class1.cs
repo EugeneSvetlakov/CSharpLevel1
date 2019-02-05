c1ff916c1ee7d9f182c7c01ca6b4e326ab357d8f
EugeneSvetlakov <eugene.svetlakov@gmail.com>
2019-02-05 23:16:16 +0300

    ЗД урок 6. Задача 2 (частично решена)

---
+++ lesson6/Lib.FindMin/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lib.FindMin
+{
+    public delegate double Fx(double x);
+    public class FindMin
+    {
+
+        public static double Fxx(double x)
+        {
+            return x * x;
+        }
+        public static double X(double x)
+        {
+            return x;
+        }
+        /// <summary>
+        /// FindM
+        /// </summary>
+        /// <param name="F">Функция</param>
+        /// <param name="x1">начало диаппазона</param>
+        /// <param name="x2">конец диаппазона</param>
+        /// <param name="dx">шаг</param>
+        public static double MinF(Fx F, double x1, double x2, double dx)
+        {
+            double xx = x1;
+            double minF = F(xx);
+            double y = 0;
+            while (xx <= x2)
+            {
+                y = F(xx);
+                if (minF > y) minF = y;
+                //System.Console.WriteLine($"xx= {xx}\tF(x)= {Math.Round(y,4)}");
+                xx = Math.Round(xx+dx,2);
+            }
+            return Math.Round(minF,4);
+        }
+        
+    }
+}
---
+++ lesson6/lev1les6t2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Lib.FindMin;
+
+//Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата. 
+// а) Сделать меню с различными функциями и представить пользователю выбор, 
+//для какой функции и на каком отрезке находить минимум. 
+//Использовать массив (или список) делегатов, в котором хранятся различные функции.
+// б) *Переделать функцию Load, чтобы она возвращала массив считанных значений. 
+//Пусть она возвращает минимум через параметр (с использованием модификатора out).
+
+namespace lev1les6t2
+{
+    class Program
+    {
+        public static void MyMenu()
+        {
+            Fx f;
+            double x1;
+            double x2;
+            double dx;
+            Console.WriteLine("Программа поиска минимума функции.");
+            while (true)
+            {
+                System.Console.WriteLine("Выберите одну из предложенных функций:");
+                System.Console.WriteLine("1. f(x) = x; 2. f(x)=x*x; 3. f(x)=sin(x); 4. f(x)=cosh(x): ");
+                if (int.TryParse(Console.ReadLine(), out int ans) && ans >= 1 && ans <= 4)
+                {
+                    switch (ans)
+                    {
+                        case 1:
+                            f = new Fx(FindMin.X);
+                            break;
+                        case 2:
+                            f = new Fx(FindMin.Fxx);
+                            break;
+                        case 3:
+                            f = new Fx(Math.Sin);
+                            break;
+                        case 4:
+                        default:
+                            f = new Fx(Math.Cosh);
+                            break;
+                    }
+                    break;
+                }
+            }
+            while (true)
+            {
+                System.Console.WriteLine("х_min: ");
+                if (double.TryParse(Console.ReadLine(), out double result))
+                {
+                    x1 = result;
+                    break;
+                }
+            }
+            while (true)
+            {
+                System.Console.WriteLine("х_max: ");
+                if (double.TryParse(Console.ReadLine(), out double result) && result > x1)
+                {
+                    x2 = result;
+                    break;
+                }
+            }
+            while (true)
+            {
+                System.Console.WriteLine("шаг: ");
+                if (double.TryParse(Console.ReadLine(), out double result) && result <= (x2 - x1) / 2)
+                {
+                    dx = result;
+                    break;
+                }
+            }
+            System.Console.WriteLine($"x_min={x1}, x_max={x2}, dx={dx}, Минимум = {FindMin.MinF(f, x1, x2, dx)}");
+        }
+        static void Main(string[] args)
+        {
+            // Fx f1 = new Fx(FindMin.X);
+            // Fx f2 = new Fx(FindMin.Fxx);
+            // Fx f3 = new Fx(Math.Sin);
+            // Fx f4 = new Fx(Math.Cosh);
+            // System.Console.WriteLine($"Min(f1) = {FindMin.MinF(f1, -2,2,0.1)}");
+            // System.Console.WriteLine($"Min(f2) = {FindMin.MinF(f2, -2,2,0.1)}");
+            // System.Console.WriteLine($"Min(f3) = {FindMin.MinF(f3, -2,2,0.1)}");
+            // System.Console.WriteLine($"Min(f4) = {FindMin.MinF(f4, -2,2,0.1)}");
+            MyMenu();
+
+        }
+    }
+}
